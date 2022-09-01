cec11a9285eff2b6d2b01117bee1b0f02bdcaf4b
likagogishvili <likagogishvili2@gmail.com>
2022-09-01 13:11:28 +0400

    adding sign in

--- client/src/SIgnIn/SignIn.js
+++ client/src/SIgnIn/SignIn.js
@@ -1,61 +1,85 @@
-import {useState} from "react";
+import { useState } from "react";
 import "./signIn.css";
 import axios from "axios";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 function SignIn(props) {
   const [userName, SetUserName] = useState("");
   const [password, SetPassword] = useState("");
   let navigate = useNavigate();
-  const [errorText, SetErrorText] = useState('')
+  const [errorText, SetErrorText] = useState("");
   function SubmitSignIn(event) {
     event.preventDefault();
     let baseURL = `http://localhost:4000/user/${userName}/${password}`;
     axios.get(baseURL).then((response) => {
       if (response.data) {
         props.SetUserData(response.data);
-        if(response.data.length){
-          navigate('/Chanacvleba', { replace: true })
-          SetErrorText("")
+        if (response.data.length) {
+          navigate("/Chanacvleba", { replace: true });
+          SetErrorText("");
         }
-      } if(response.data.length === 0) {
-        SetErrorText("გთხოვთ შეიყვანოთ სწორი მონაცემები")
+      }
+      if (response.data.length === 0) {
+        SetErrorText("გთხოვთ შეიყვანოთ სწორი მონაცემები");
       }
     });
   }
 
-
   return (
-    <div className="main_container">
-      <div className="container">
-        <h1 className="h1_text">ავტორიზაცია</h1>
-        <div className="sign_in_container">
-          <input
-            className="input"
-            type="text"
-            placeholder="სახელი"
-            name="uname"
-            required
-            value={userName}
-            onChange={(e) => SetUserName(e.target.value)}
-          />
+    <section className="vh-100" style={{ backgroundColor: "#F8F9FA" }}>
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div
+              className="card shadow-2-strong"
+              style={{ borderRadius: "1rem" }}
+            >
+              <div className="card-body p-5 text-center">
+                <h3 className="mb-5">ავტორიზაცია</h3>
 
-          <input
-            className="input"
-            type="password"
-            placeholder="პაროლი"
-            name="psw"
-            required
-            value={password}
-            onChange={(e) => SetPassword(e.target.value)}
-          />
-          <p style={{ color: "red" }}>{errorText}</p>
-          <button className="sign_in_btn" type="submit" onClick={SubmitSignIn}>
-            შესვლა
-          </button>
+                <div className="form-outline mb-4">
+                  <input
+                    type="name"
+                    id="typeEmailX-2"
+                    className="form-control form-control-lg"
+                    value={userName}
+                    onChange={(e) => SetUserName(e.target.value)}
+                  />
+                  <label className="form-label" htmlFor="typeEmailX-2">
+                    სახელი
+                  </label>
+                </div>
+
+                <div className="form-outline mb-4">
+                  <input
+                    type="password"
+                    id="typePasswordX-2"
+                    className="form-control form-control-lg"
+                    value={password}
+                    onChange={(e) => SetPassword(e.target.value)}
+                  />
+                  <label className="form-label" htmlFor="typePasswordX-2">
+                    პაროლი
+                  </label>
+                </div>
+                <p style={{ color: "red" }}>{errorText}</p>
+
+                <button
+                  className="btn btn-success btn-lg btn-block mb-2"
+                  type="submit"
+                  style={{ width: "100%" }}
+                  onClick={SubmitSignIn}
+                >
+                  შესვლა
+                </button>
+
+                <hr className="my-4" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 export default SignIn;
